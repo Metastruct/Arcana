520497362894465b812820a629ca0d6120a14702
Ryan <ryan.rbsr@gmail.com>
2025-10-26 19:50:56 +0100

    spawn at least one graveyard

--- lua/arcana/environments/magical_forest.lua
+++ lua/arcana/environments/magical_forest.lua
@@ -620,7 +620,7 @@ Envs:RegisterEnvironment({
 	pois = {
 		{ id = "mushroom_hotspot", spawn = spawnMushroomHotspot, max = 2, min = 1 },
 		{ id = "fairy_grove", spawn = spawnFairyGrove, max = 1, min = 1 },
-		{ id = "graveyard", spawn = spawnGraveyard, max = 3, min = 0 },
+		{ id = "graveyard", spawn = spawnGraveyard, max = 3, min = 1 },
 	},
 })
 

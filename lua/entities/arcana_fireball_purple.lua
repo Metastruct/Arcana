ea2d04449f0b897568b6f04c3113abb39939a93f
Ryan <ryan.rbsr@gmail.com>
2025-11-10 13:48:20 +0100

    fix inverted inflictor and attacker

--- lua/entities/arcana_fireball_purple.lua
+++ lua/entities/arcana_fireball_purple.lua
@@ -111,7 +111,7 @@ if SERVER then
 
 		local owner = self:GetSpellOwner() or self
 		local pos = self:GetPos()
-		Arcane:BlastDamage(self, IsValid(owner) and owner or self, pos, self.FireballRadius, self.FireballDamage, DMG_BLAST, true, true)
+		Arcane:BlastDamage(IsValid(owner) and owner or self, self, pos, self.FireballRadius, self.FireballDamage, DMG_BLAST, true, true)
 
 		for _, v in ipairs(ents.FindInSphere(pos, self.FireballRadius)) do
 			if IsValid(v) and (v:IsPlayer() or v:IsNPC()) then

e6c74c63a7c92507dd365a62ab623b8d41d61d2f
Ryan <ryan.rbsr@gmail.com>
2025-12-23 22:55:05 +0100

    there is still problems with pac but thats better

--- lua/arcana/volumetric_fog.lua
+++ lua/arcana/volumetric_fog.lua
@@ -170,7 +170,7 @@ end
 -- Render all active fog volumes
 local hasGShader = file.Exists("materials/pp/wp_reconstruction.vmt", "GAME")
 local hasNoiseTexture = file.Exists("materials/arcana/mercfog3dnoise.vtf", "GAME")
-hook.Add("PreDrawTranslucentRenderables", "Arcana_VolumetricFog", function()
+hook.Add("PostDrawReconstruction", "Arcana_VolumetricFog", function(isDrawingDepth, isDrawSkybox, isDraw3DSkybox)
 	if not hasGShader or not hasNoiseTexture then return end
 
 	for id, vol in pairs(activeVolumes) do
